7f79b07896424bc0ecd35239c35d1490c83e6f94
Frank Pommerening <frank@pommerening-online.de>
2021-07-18 16:40:24 +0200

    Add duplex sample

---
+++ Duplex/Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using FP.gRPCdoetnet.Streaming.Duplex.Contract;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace FP.gRPCdoetnet.Streaming.Simplex.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new StreamingServices.StreamingServicesClient(channel);
+
+            try
+            {
+                using (var streamingCall = client.SendMultiEcho())
+                {
+                    var sendTask = SendMessage(streamingCall);
+                    var receiveTask = ReceiveEcho(streamingCall);
+                    await Task.WhenAll(sendTask, receiveTask);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Server close streaming
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error on send messages \n {e}");
+            }
+        }
+
+        private static async Task SendMessage(AsyncDuplexStreamingCall<EchoMessage, EchoMessage> streamingCall)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                await streamingCall.RequestStream.WriteAsync(new EchoMessage
+                {
+                    Content = $"Hallo server {i}",
+                    Timestamp = DateTime.UtcNow.ToTimestamp()
+                });
+                await Task.Delay(500);
+            }
+
+            await streamingCall.RequestStream.CompleteAsync();
+        }
+
+        private static async Task ReceiveEcho(AsyncDuplexStreamingCall<EchoMessage, EchoMessage> streamingCall)
+        {
+            await foreach (var response in streamingCall.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{response.Timestamp.ToDateTime():G}: {response.Content}");
+            }
+        }
+    }
+}
---
+++ Duplex/Server/Services/StreamingService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using FP.gRPCdoetnet.Streaming.Duplex.Contract;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace FP.gRPCdoetnet.Streaming.Simplex.Server.Services
+{
+    public class StreamingService : StreamingServices.StreamingServicesBase
+    {
+        private readonly ILogger<StreamingService> _logger;
+        private readonly ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+
+        public StreamingService(ILogger<StreamingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task SendMultiEcho(IAsyncStreamReader<EchoMessage> requestStream, IServerStreamWriter<EchoMessage> responseStream, ServerCallContext context)
+        {
+            var receiveTask =  ReceiveMessageAsync(requestStream, context);
+
+            var sendTask = SendMessageFromQueueAsync(responseStream, context);
+
+            await Task.WhenAll(receiveTask, sendTask);
+        }
+
+        private async Task SendMessageFromQueueAsync(IServerStreamWriter<EchoMessage> responseStream, ServerCallContext context)
+        {
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+                if (_queue.IsEmpty) continue;
+                if (_queue.TryDequeue(out var msg))
+                {
+                    await responseStream.WriteAsync(new EchoMessage
+                    {
+                        Content = $"echo {msg}",
+                        Timestamp = DateTime.UtcNow.ToTimestamp()
+                    });
+                }
+            }
+        }
+
+        private async Task ReceiveMessageAsync(IAsyncStreamReader<EchoMessage> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+            {
+                var msg = requestStream.Current;
+                _logger.LogInformation($"getting message '{msg.Content}' with timestamp {msg.Timestamp.ToDateTime():G}");
+                _queue.Enqueue(msg.Content);
+            }
+        }
+    }
+}
