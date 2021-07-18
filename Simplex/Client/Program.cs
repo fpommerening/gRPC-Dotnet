ea57651e719dc8e7f338802139b3117ae6099a77
Frank Pommerening <frank@pommerening-online.de>
2021-07-18 15:39:31 +0200

    Add simplex streaming

---
+++ Simplex/Client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using FP.gRPCdoetnet.Streaming.Simplex.Contract;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace FP.gRPCdoetnet.Streaming.Simplex.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new StreamingServices.StreamingServicesClient(channel);
+            //await SendLogs(client);
+            await GetTime(client);
+
+
+        }
+
+        private static async Task SendLogs(StreamingServices.StreamingServicesClient client)
+        {
+            try
+            {
+                using (var streamingCall = client.SendLog())
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        await streamingCall.RequestStream.WriteAsync(new LogRequest
+                        {
+                            Message = $"Log {i}",
+                            Timestamp = DateTime.UtcNow.ToTimestamp()
+                        });
+                        
+                    }
+                    await streamingCall.RequestStream.CompleteAsync();
+                    var result = await streamingCall.ResponseAsync;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Server close streaming
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error on send logs \n {e}");
+            }
+        }
+
+        private static async Task GetTime(StreamingServices.StreamingServicesClient client)
+        {
+            try
+            {
+                var request = new GetTimeRequest()
+                {
+                    Count = 10,
+                    Name = "Frank"
+                };
+                using (var streamingCall = client.GetTime(request))
+                {
+                    await foreach (var response in streamingCall.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine($"Time from server {response.Timestamp.ToDateTime():G}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Server close streaming
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error on receive time \n {e}");
+            }
+        }
+    }
+}
---
+++ Simplex/Server/Services/StreamingService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using FP.gRPCdoetnet.Streaming.Simplex.Contract;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace FP.gRPCdoetnet.Streaming.Simplex.Server.Services
+{
+    public class StreamingService : StreamingServices.StreamingServicesBase
+    {
+        private readonly ILogger<StreamingService> _logger;
+
+        public StreamingService(ILogger<StreamingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<Empty> SendLog(IAsyncStreamReader<LogRequest> requestStream, ServerCallContext context)
+        {
+            try
+            {
+                // need c# 8
+                await foreach (var request in requestStream.ReadAllAsync())
+                {
+                    _logger.LogInformation($"{request.Timestamp.ToDateTime():G} - {request.Message}");
+                }
+                
+            }
+            catch (OperationCanceledException e)
+            {
+                // Operation was canceled
+            }
+            catch (RpcException rpcEx)
+            {
+                if (rpcEx.StatusCode == StatusCode.Cancelled)
+                {
+                    // Operation was canceled
+                }
+                else
+                {
+                    _logger.LogError(rpcEx, "Unexpected error on receive logs");
+                }
+            }
+            return new Empty();
+        }
+
+        public override async Task GetTime(GetTimeRequest request, IServerStreamWriter<GetTimeResponse> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation($"Sending {request.Count} responses to {request.Name}");
+
+            for (var i = 0; i < request.Count; i++)
+            {
+                await responseStream.WriteAsync(new GetTimeResponse {Timestamp = DateTime.UtcNow.ToTimestamp()});
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+        }
+    }
+}
