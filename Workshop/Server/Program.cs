9576d0afb55ddaf0d792176837a51085b32cd657
Frank Pommerening <frank@pommerening-online.de>
2024-02-17 18:31:17 +0100

    Add grpc metrics interceptor

--- Workshop/Server/Program.cs
+++ Workshop/Server/Program.cs
@@ -1,13 +1,22 @@
 using FP.gRPCdotnet.Workshop.Server.Business;
 using FP.gRPCdotnet.Workshop.Server.Services;
-using Prometheus;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
+        meterProviderBuilder.AddMeter(MeticsInterceptor.Metrics.Name);
+    });
+
 
 // Add services to the container.
 builder.Services.AddGrpc(options =>
 {
-    options.Interceptors.Add(typeof(RpcExceptionWrapperInterceptor));
+    options.Interceptors.Add<MeticsInterceptor>();
+    options.Interceptors.Add<RpcExceptionWrapperInterceptor>();
+
     // options.EnableDetailedErrors = true;
 });
 
@@ -15,10 +24,9 @@ builder.Services.AddSingleton(new WorkshopRepository());
 
 var app = builder.Build();
 
-app.UseGrpcMetrics();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<WorkshopService>();
-app.MapMetrics();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ Workshop/Server/Services/MetricsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace FP.gRPCdotnet.Workshop.Server.Services;
+
+public static class MetricsExtensions
+{
+    public static async Task<T> MeterDuration<T>(this Histogram<double> instance, Func<Task<T>> call)
+    {
+        return await instance.MeterDuration(call, new TagList());
+    }
+
+    public static async Task<T> MeterDuration<T>(this Histogram<double> instance, Func<Task<T>> call, TagList tagList)
+    {
+        var timer = new Stopwatch();
+        try
+        {
+            timer.Start();
+            return await call();
+        }
+        finally
+        {
+            timer.Stop();
+            instance.Record(timer.Elapsed.TotalMilliseconds, tagList);
+        }
+    }
+}
