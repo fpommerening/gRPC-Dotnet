0d34f0df52774c5e0f5826207f36a5219229425e
Frank Pommerening <frank@pommerening-online.de>
2024-02-19 21:07:38 +0100

    Add prepare

--- Workshop/Client/Program.cs
+++ Workshop/Client/Program.cs
@@ -3,19 +3,7 @@ using Grpc.Net.Client;
 
 try
 {
-    var channel = GrpcChannel.ForAddress("http://localhost:5002");
-    var client = new WorkshopServices.WorkshopServicesClient(channel);
-    var workshops = await client.GetWorkshopsAsync(new GetWorkshopsRequest());
-    // client.AddAttendee(new AddAttendeeRequest
-    // {
-    //     Person = new Person
-    //     {
-    //         Name = "Mustermann",
-    //         Firstname = "Sabine",
-    //         Email = "smustermann@demo-apps.de"
-    //     },
-    //     Id = 42
-    // }, new Metadata());
+  
 }
 catch(Exception ex)
 {
--- Workshop/Server/Program.cs
+++ Workshop/Server/Program.cs
@@ -3,30 +3,18 @@ using FP.gRPCdotnet.Workshop.Server.Services;
 using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenTelemetry()
-    .WithMetrics(meterProviderBuilder =>
-    {
-        meterProviderBuilder.AddPrometheusExporter();
-        meterProviderBuilder.AddMeter(MeticsInterceptor.Metrics.Name);
-    });
+//builder.Services.AddOpenTelemetry();
 
 
 // Add services to the container.
-builder.Services.AddGrpc(options =>
-{
-    options.Interceptors.Add<MeticsInterceptor>();
-    options.Interceptors.Add<RpcExceptionWrapperInterceptor>();
-
-    // options.EnableDetailedErrors = true;
-});
+builder.Services.AddGrpc();
 
 builder.Services.AddSingleton(new WorkshopRepository());
 
 var app = builder.Build();
 
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
-// Configure the HTTP request pipeline.
-app.MapGrpcService<WorkshopService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
